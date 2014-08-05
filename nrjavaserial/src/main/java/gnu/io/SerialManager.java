6947f99bf5c0e77f7c250e5fa9f691863b7d4b59
Andreas Kuhtz <andreas.kuhtz@gmail.com>
2014-08-05 17:53:47 +0200

    Use slf4j instead of sysout

--- nrjavaserial/src/main/java/gnu/io/SerialManager.java
+++ nrjavaserial/src/main/java/gnu/io/SerialManager.java
@@ -1,20 +1,29 @@
 package gnu.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SerialManager {
-	
-	private static SerialManager instance;
-	private static boolean loaded = false;
-	private SerialManager() throws NativeResourceException {
-		if(!loaded) {
-			loaded = true;
-			new NativeResource().load("libNRJavaSerial");	
-		}
-	}
-	
-	public static SerialManager getInstance() throws NativeResourceException {
-		if(instance == null) {
-			instance = new SerialManager();
-		}		
-		return instance;
-	}
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SerialManager.class);
+
+    private static SerialManager instance;
+
+    private static boolean loaded = false;
+
+    private SerialManager() throws NativeResourceException {
+        if (!loaded) {
+            loaded = true;
+
+            LOGGER.info("Load native library.");
+            new NativeResource().load("libNRJavaSerial");
+        }
+    }
+
+    public static SerialManager getInstance() throws NativeResourceException {
+        if (instance == null) {
+            instance = new SerialManager();
+        }
+        return instance;
+    }
 }
